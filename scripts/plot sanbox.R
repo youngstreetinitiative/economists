daaa4749bc1482a31b122e2216c21a4eeb622fa8
rosseji <rosseireland@gmail.com>
2017-07-24 08:57:13 +1000

    sandbox

--- scripts/plot sanbox.R
+++ scripts/plot sanbox.R
@@ -1,19 +1,34 @@
+library(ggrepel)
+
+end.points.data <- tibble(full.nm = c("Loose Monetary", "Open the floodgates",
+                                      "Tight Monetary", "Gold Only",
+                                      "Loose Fiscal", "Tight Fiscal"),
+                          abr.nm = c("Loose Monetary", "Open the floodgates",
+                                     "Tight Monetary", "Gold Only",
+                                     "Loose Fiscal", "Tight Fiscal"),
+                          pol.fis = c(0, 0, 0, 0, -1, 1),
+                          pol.mon = c(1, 0.9, -0.9, -1, 0, 0))
 
 
-ggplot(econs, aes(x= pol.fis, y = pol.mon, col= as.factor(full.nm))) +
-  geom_hline(yintercept = 0, alpha = 0.2) +
-  geom_vline(xintercept = 0, alpha = 0.2) +
+ggplot(econs, aes(x =  pol.fis, y = pol.mon, col = full.nm)) +
+  annotate("segment", x = 0, xend = 0, y = 1, yend = -1) +
+  annotate("segment", x = 1, xend = -1, y = 0, yend = 0) +
   geom_point(show.legend = F) +
-  geom_text(data = econs, aes(label=full.nm),hjust=-0.2, vjust=0, show.legend = F) +
+  geom_label_repel(aes(label = full.nm), show.legend = F) +
   ggtitle("Comparing Economists on Fiscal and Monetary Policy") +
-  scale_y_discrete("Monetary Policy", limits = c(-1,1)) +
-  scale_x_discrete("Fiscal Policy", limits = c(-1,1)) +
-  theme(axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks.y=element_blank()) +
-  geom_point(data = end.points.data, aes(x = x, y = y)) +
-  geom_text(data = end.points.data, aes(label=lab), hjust=-0.2, vjust=0, show.legend = F)
-
-
-end.points.data <- tibble(lab = c("Both Tight", "Tight Monetary", "Both Loose", "Tight Fiscal"), x= c(1, -1, 1, -1), y = c(1, 1, -1, -1))
+  theme(axis.text.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.title.y = element_blank(),
+        axis.ticks.y = element_blank()) +
+  annotate("text", x = 0, y = 1.2, label = "Loose Monetary") +
+  annotate("text", x = 0, y = 1.1, label = '"Open the floodgates"') +
+  annotate("text", x = 0, y = -1.1, label = "Tight Monetary") +
+  annotate("text", x = 0, y = -1.2, label = '"Gold Only"') +
+  annotate("text", x = -1.1, y = 0, label = "Loose Fiscal", angle = 90) +
+  annotate("text", x = 1.1, y = 0, label = "Tight Fiscal", angle = 270) +
+  annotate("segment", x = -1, xend = -1, y = 1, yend = -1) +
+  annotate("segment", x = 1, xend = 1, y = 1, yend = -1) +
+  annotate("segment", x = -1, xend = 1, y = -1, yend = -1) +
+  annotate("segment", x = 1, xend = -1, y = 1, yend = 1)
